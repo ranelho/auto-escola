b561d1e8d2b88e53cbfc1302d68cab2b3a168444
ranelho <ranelhol@gmail.com>
2023-03-29 16:27:33 -0300

    implementa contato

---
+++ src/main/java/com/rlti/autoescola/contato/domain/Contato.java
@@ -0,0 +1,29 @@
+package com.rlti.autoescola.contato.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class Contato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idContato;
+    private String telefone;
+    private String cep;
+    private String endereco;
+    private String cidade;
+    private String uf;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id")
+    @JsonIgnore
+    private Cliente cliente;
+}
