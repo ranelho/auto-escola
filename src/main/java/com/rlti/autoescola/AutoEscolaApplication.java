b4d570670884d9e03eb37c3e1410a7be0dea83d4
ranelho <ranelhol@gmail.com>
2023-03-29 16:04:50 -0300

    configura projeto

---
+++ src/main/java/com/rlti/autoescola/AutoEscolaApplication.java
@@ -0,0 +1,13 @@
+package com.rlti.autoescola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoEscolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoEscolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rlti/autoescola/AutoEscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlti.autoescola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoEscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
