a4759bcd48eb05a67042dfb328b9d50f1f0db3ad
ranelho <ranelhol@gmail.com>
2023-03-29 16:27:25 -0300

    implementa cliente

---
+++ src/main/java/com/rlti/autoescola/cliente/domain/Cliente.java
@@ -0,0 +1,35 @@
+package com.rlti.autoescola.cliente.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.rlti.autoescola.contato.domain.Contato;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idCliente;
+    @CPF
+    private String cpf;
+    @NotNull
+    private String firstName;
+    private String lastName;
+    @Enumerated(EnumType.STRING)
+    private EstadoCivil estadoCivil;
+
+    @OneToMany(cascade=CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "cliente")
+    @JsonIgnore
+    List<Contato> contato;
+}
