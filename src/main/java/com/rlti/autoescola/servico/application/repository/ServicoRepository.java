458a2f99dc9f45fc61d418a1b286978a5118a66a
ranelho <ranelhol@gmail.com>
2023-03-31 11:20:53 -0300

    implementa repository

--- src/main/java/com/rlti/autoescola/servico/application/repository/ServicoRepository.java
+++ src/main/java/com/rlti/autoescola/servico/application/repository/ServicoRepository.java
@@ -2,6 +2,9 @@ package com.rlti.autoescola.servico.application.repository;
 
 import com.rlti.autoescola.servico.domain.Servico;
 
+import java.util.UUID;
+
 public interface ServicoRepository {
     Servico salva(Servico servico);
+    Servico getById(UUID idServico);
 }
--- src/main/java/com/rlti/autoescola/servico/infra/ServicoInfraRepository.java
+++ src/main/java/com/rlti/autoescola/servico/infra/ServicoInfraRepository.java
@@ -1,10 +1,15 @@
 package com.rlti.autoescola.servico.infra;
 
+import com.rlti.autoescola.handler.APIException;
 import com.rlti.autoescola.servico.application.repository.ServicoRepository;
 import com.rlti.autoescola.servico.domain.Servico;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
@@ -17,6 +22,18 @@ public class ServicoInfraRepository implements ServicoRepository {
         log.info("[inicia] ServicoInfraRepository - salva");
         servicoSpringDataInfraRepository.save(servico);
         log.info("[finaliza] ServicoInfraRepository - salva");
+        return servico;
+    }
+
+    @Override
+    public Servico getById(UUID idServico) {
+        log.info("[inicia] ServicoInfraRepository - getById");
+        Optional<Servico> optionalServico = servicoSpringDataInfraRepository.findById(idServico);
+        Servico servico = optionalServico
+                .orElseThrow(
+                        () -> APIException.build(HttpStatus.BAD_REQUEST, "Serviço não encontrado")
+                );
+        log.info("[finaliza] ServicoInfraRepository - getById");
         return servico;
     }
 }
