43a4602ee58efffc78ec5913bcb57adda1e5e756
ranelho <ranelhol@gmail.com>
2023-04-03 17:20:23 -0300

    implementa controller

---
+++ src/main/java/com/rlti/autoescola/frota/manutencao/application/api/ManutencaoApi.java
@@ -0,0 +1,15 @@
+package com.rlti.autoescola.frota.manutencao.application.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/v1/manutencao")
+public interface ManutencaoApi {
+
+    @PostMapping("/{placa}")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    ManutencaoIdResponse novaManutencao(@PathVariable String placa, @Valid @RequestBody ManutencaoRequest request);
+}
---
+++ src/main/java/com/rlti/autoescola/frota/manutencao/application/repository/ManutencaoRepository.java
@@ -0,0 +1,7 @@
+package com.rlti.autoescola.frota.manutencao.application.repository;
+
+import com.rlti.autoescola.frota.manutencao.Manutencao;
+
+public interface ManutencaoRepository {
+    Manutencao salva(Manutencao manutencao);
+}
