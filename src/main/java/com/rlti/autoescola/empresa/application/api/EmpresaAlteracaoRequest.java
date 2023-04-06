1b7aeac650dc330e9634371d3dbe81551fc0b63b
Ranelho Lacerda <ranelhol@gmail.com>
2023-04-06 17:14:48 -0300

    valida cnpj empresa

--- src/main/java/com/rlti/autoescola/empresa/application/api/EmpresaAlteracaoRequest.java
+++ src/main/java/com/rlti/autoescola/empresa/application/api/EmpresaAlteracaoRequest.java
@@ -5,6 +5,7 @@ import lombok.Value;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 @Value
 public class EmpresaAlteracaoRequest {
@@ -19,6 +20,7 @@ public class EmpresaAlteracaoRequest {
     @NotBlank
     @Email
     String email;
+    @Pattern(regexp = "^\\(\\d{2}\\)\\d{5}-\\d{4}$")
     String telefone;
     @NotNull
     String enderecoComercial;
