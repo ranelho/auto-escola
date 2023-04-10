70e2e56e8742e31c811ff8a3e6ef6082adc3aa77
Ranelho Lacerda <ranelhol@gmail.com>
2023-04-10 13:33:54 -0300

    valida categoria

--- src/main/java/com/rlti/autoescola/matricula/domain/TipoServico.java
+++ src/main/java/com/rlti/autoescola/matricula/domain/TipoServico.java
@@ -1,5 +1,30 @@
 package com.rlti.autoescola.matricula.domain;
 
+import com.rlti.autoescola.servico.domain.Categoria;
+
 public enum TipoServico {
-    PRIMEIRA_HABILITACAO, ADICAO_CATEGORIA, RENOVACAO, RECICLAGEM
+    PRIMEIRA_HABILITACAO, ADICAO_CATEGORIA, RENOVACAO, RECICLAGEM, REEXAME;
+
+    public boolean isValidCategoria(Categoria categoria) {
+        switch (this) {
+            case PRIMEIRA_HABILITACAO:
+                return categoria == Categoria.ACC || categoria == Categoria.A || categoria == Categoria.B || categoria == Categoria.AB;
+            case ADICAO_CATEGORIA:
+                switch (categoria) {
+                    case A:
+                    case B:
+                        return true;
+                    case C:
+                        return this.isValidCategoria(Categoria.B);
+                    case D:
+                        return this.isValidCategoria(Categoria.B) || this.isValidCategoria(Categoria.C);
+                    case E:
+                        return this.isValidCategoria(Categoria.D);
+                    default:
+                        return false;
+                }
+            default:
+                return true;
+        }
+    }
 }
--- src/main/java/com/rlti/autoescola/servico/domain/Categoria.java
+++ src/main/java/com/rlti/autoescola/servico/domain/Categoria.java
@@ -1,5 +1,30 @@
 package com.rlti.autoescola.servico.domain;
 
+import com.rlti.autoescola.matricula.domain.TipoServico;
+
 public enum Categoria {
-    A, B, C, D, E, AB, AC, AD, AE, RENOVACAO, REEXAME
+    ACC, A, B, C, D, E, AB;
+
+    public boolean isValidTipoServico(TipoServico tipoServico) {
+        switch (tipoServico) {
+            case PRIMEIRA_HABILITACAO:
+                return this == Categoria.ACC || this == Categoria.A || this == Categoria.B || this == Categoria.AB;
+            case ADICAO_CATEGORIA:
+                switch (this) {
+                    case A:
+                    case B:
+                        return true;
+                    case C:
+                        return tipoServico.isValidCategoria(Categoria.B);
+                    case D:
+                        return tipoServico.isValidCategoria(Categoria.B) || tipoServico.isValidCategoria(Categoria.C);
+                    case E:
+                        return tipoServico.isValidCategoria(Categoria.D);
+                    default:
+                        return false;
+                }
+            default:
+                return true;
+        }
+    }
 }
