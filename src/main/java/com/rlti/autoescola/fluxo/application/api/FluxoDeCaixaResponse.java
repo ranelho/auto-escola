2104cde2303bf215cbbfd15ea3bdbf8ad84c89f5
ranelho <ranelhol@gmail.com>
2023-04-25 17:28:38 -0300

    atualiza fluxo de caixa

--- src/main/java/com/rlti/autoescola/fluxo/application/api/FluxoDeCaixaResponse.java
+++ src/main/java/com/rlti/autoescola/fluxo/application/api/FluxoDeCaixaResponse.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 public class FluxoDeCaixaResponse {
     List<PagamentoFluxoResponse> receitas;
     List<ManutencaoFluxoResponse> despesas;
-    List<ReceitaPagamentoResponse> receitaPagamentoResponseList;
+    List<ReceitaPagamentoResponse> receitarPorTipoPagamento;
     BigDecimal totalReceitas;
     BigDecimal totalDespesas;
     BigDecimal saldo;
@@ -22,7 +22,7 @@ public class FluxoDeCaixaResponse {
         this.saldo = fluxo.getSaldoGeral();
         this.totalReceitas = fluxo.getValorTotalReceitas();
         this.totalDespesas = fluxo.getValorTotalDespesas();
-        this.receitaPagamentoResponseList = fluxo.getReceitaPagamentoResponseList();
+        this.receitarPorTipoPagamento = fluxo.getReceitaPagamentoResponseList();
     }
 
     public static List<FluxoDeCaixaResponse> converte(List<Fluxo> fluxo) {
