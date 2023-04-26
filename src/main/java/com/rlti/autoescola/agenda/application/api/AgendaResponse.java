de1c5a9e5dab2f0de3f23633a023a25a4a8878bc
matheusprata <matheusprata.ti@gmail.com>
2023-04-25 22:26:45 -0300

    adicionado id ao response para localizar o instrutor, matricula e veiculo

--- src/main/java/com/rlti/autoescola/agenda/application/api/AgendaResponse.java
+++ src/main/java/com/rlti/autoescola/agenda/application/api/AgendaResponse.java
@@ -6,16 +6,25 @@ import com.rlti.autoescola.agenda.domain.TipoAula;
 import lombok.Value;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Value
 public class AgendaResponse {
+    Long idAgenda;
     LocalDate data;
     HorarioAula horarioAula;
     TipoAula tipoAula;
+    UUID idInstrutor;
+    UUID idMatricula;
+    String placa;
 
     public AgendaResponse(Agenda agenda) {
+        this.idAgenda = agenda.getIdAgenda();
         this.data = agenda.getData();
         this.horarioAula = agenda.getHorarioAula();
         this.tipoAula = agenda.getTipoAula();
+        this.idInstrutor = agenda.getInstrutor().getIdInstrutor();
+        this.idMatricula = agenda.getMatricula().getIdMatricula();
+        this.placa = agenda.getVeiculo().getPlaca();
     }
 }
