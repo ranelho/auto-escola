dd6c01ec2be5f57ba5d98ab280c75c6fa73d9c04
ranelho <ranelhol@gmail.com>
2023-04-27 15:20:15 -0300

    valida exame 3

--- src/main/java/com/rlti/autoescola/exame/domain/Validate.java
+++ src/main/java/com/rlti/autoescola/exame/domain/Validate.java
@@ -12,36 +12,34 @@ import static com.rlti.autoescola.exame.domain.TipoExame.*;
 public class Validate {
     public static void validaExame(List<Exame> exames, ExameRequest request)  {
         if (exames.isEmpty() && request.getTipoExame() != CLINICO) {
-            throw APIException
-                    .build(HttpStatus.BAD_REQUEST,"O primeiro exame deve ser do tipo CLINICO");
+            throw APIException.build(HttpStatus.BAD_REQUEST,"O primeiro exame deve ser do tipo CLINICO");
         }
         for (Exame exame : exames) {
-            if (exame.getTipoExame() == CLINICO && exame.getResultado() == INAPTO
-                    && request.getTipoExame() == CLINICO) {
+            //valida exame clinico
+            if (request.getTipoExame() == CLINICO && exame.getTipoExame() == CLINICO && exame.getResultado() == APTO) {
+                throw APIException.build(HttpStatus.BAD_REQUEST,"Não é permitido cadastrar outro exame CLINICO");
+            }//OK
+            if (request.getTipoExame() == TEORICO && exame.getTipoExame() == TEORICO && exame.getResultado() == APTO){
+                throw APIException.build(HttpStatus.BAD_REQUEST,"Não é permitido cadastrar outro exame TEORICO");
+            }
+            if (request.getTipoExame() == PRATICO && exame.getTipoExame() == PRATICO && exame.getResultado() == APTO){
+                throw APIException.build(HttpStatus.BAD_REQUEST,"Não é permitido cadastrar outro exame PRATICO");
+            }
+            //valida exame clinico
+            if(request.getTipoExame().equals(CLINICO)){
+                if ((exame.getTipoExame() == CLINICO && exame.getResultado() == INAPTO)
+                        || exame.getTipoExame() == CLINICO && exame.getResultado() != A_FAZER) {
+                    continue;
+                }
+            }
+            //valida exame teorico
+            if ((request.getTipoExame() == TEORICO && exame.getTipoExame() == CLINICO && exame.getResultado() == APTO) || (request.getTipoExame() == TEORICO && exame.getTipoExame() == TEORICO && exame.getResultado() == INAPTO)) {
                 continue;
             }
-            if (request.getTipoExame() == CLINICO && exame.getResultado() == APTO) {
-                throw APIException
-                        .build(HttpStatus.BAD_REQUEST,"Não é permitido cadastrar outro exame CLINICO");
-            }
-            if ((request.getTipoExame() == TEORICO && exame.getTipoExame() == CLINICO && exame.getResultado() == APTO) ||
-                    (request.getTipoExame() == TEORICO && exame.getTipoExame() == TEORICO && exame.getResultado() == INAPTO)) {
+            if ((request.getTipoExame() == PRATICO && exame.getTipoExame() == TEORICO && exame.getResultado() == APTO) || ((request.getTipoExame() == PRATICO && exame.getTipoExame() == PRATICO && exame.getResultado() == INAPTO)) ){
                 continue;
             }
-            if ((request.getTipoExame() == TEORICO && exame.getResultado() == APTO) ||
-                    (request.getTipoExame() == TEORICO && exame.getResultado() == A_FAZER)){
-                throw APIException
-                        .build(HttpStatus.BAD_REQUEST,"Não é permitido cadastrar outro exame TEORICO");
-            }
-            if ((request.getTipoExame() == PRATICO && exame.getTipoExame() == TEORICO && exame.getResultado() == APTO) ||
-                    ((request.getTipoExame() == PRATICO && exame.getTipoExame() == PRATICO && exame.getResultado() == INAPTO)) ){
-                continue;
-            }
-            if ((request.getTipoExame() == PRATICO && exame.getTipoExame() == PRATICO && exame.getResultado() == APTO) ||
-                    (request.getTipoExame() == PRATICO && exame.getTipoExame() == PRATICO && exame.getResultado() == A_FAZER)){
-                throw APIException
-                        .build(HttpStatus.BAD_REQUEST,"Não é permitido cadastrar outro exame PRATICO");
-            }
+
         }
     }
 }
