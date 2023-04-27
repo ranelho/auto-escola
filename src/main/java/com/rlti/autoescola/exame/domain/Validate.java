54d3b6e4cca485b8a6f9def8ef8101babc03ced9
ranelho <ranelhol@gmail.com>
2023-04-27 17:07:22 -0300

    valida 4

--- src/main/java/com/rlti/autoescola/exame/domain/Validate.java
+++ src/main/java/com/rlti/autoescola/exame/domain/Validate.java
@@ -11,8 +11,8 @@ import static com.rlti.autoescola.handler.APIException.*;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 public class Validate {
-    public static void validaExame(List<Exame> exames, ExameRequest request)  {
-        if (exames.isEmpty() && request.getTipoExame() != CLINICO) {
+    public static void validaExame(List<Exame> exames, ExameRequest request) {
+     /*   if (exames.isEmpty() && request.getTipoExame() != CLINICO) {
             throw build(BAD_REQUEST,"O primeiro exame deve ser do tipo CLINICO");
         }
         for (Exame exame : exames) {
@@ -42,6 +42,53 @@ public class Validate {
                 if ((exame.getTipoExame() == TEORICO && exame.getResultado() == APTO)
                     || (exame.getTipoExame() == PRATICO && exame.getResultado() == INAPTO)){
                     continue;
+                }
+            }
+        }
+    }*/
+        if (exames.isEmpty() && request.getTipoExame() != CLINICO) {
+            throw build(BAD_REQUEST, "O primeiro exame deve ser do tipo CLINICO");
+        }
+        for (Exame exame : exames) {
+            if (request.getTipoExame().equals(CLINICO)) {
+                if (exame.getTipoExame() == CLINICO) {
+                    if (exame.getResultado() == APTO) {
+                        throw build(BAD_REQUEST, "Não é permitido cadastrar outro exame CLINICO APTO");
+                    } else {
+                        continue;
+                    }
+                }
+            }
+
+            if (request.getTipoExame().equals(TEORICO)) {
+                if (exame.getTipoExame() == TEORICO) {
+                    if (exame.getResultado() == APTO || exame.getResultado() == A_FAZER) {
+                        throw build(BAD_REQUEST, "Não é permitido cadastrar outro exame TEORICO APTO ou A_FAZER");
+                    } else {
+                        continue;
+                    }
+                } else if (exame.getTipoExame() == CLINICO) {
+                    if (exame.getResultado() != APTO) {
+                        throw build(BAD_REQUEST, "Para cadastrar um exame TEORICO é necessário estar APTO em um exame CLINICO");
+                    } else {
+                        continue;
+                    }
+                }
+            }
+
+            if (request.getTipoExame().equals(PRATICO)) {
+                if (exame.getTipoExame() == PRATICO) {
+                    if (exame.getResultado() == APTO || exame.getResultado() == A_FAZER) {
+                        throw build(BAD_REQUEST, "Não é permitido cadastrar outro exame PRATICO APTO ou A_FAZER");
+                    } else {
+                        continue;
+                    }
+                } else if (exame.getTipoExame() == TEORICO) {
+                    if (exame.getResultado() != APTO) {
+                        throw build(BAD_REQUEST, "Para cadastrar um exame PRATICO é necessário estar APTO em um exame TEORICO");
+                    } else {
+                        continue;
+                    }
                 }
             }
         }
