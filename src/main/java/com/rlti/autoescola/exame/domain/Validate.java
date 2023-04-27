a48165966cb02c88731e86290c3f2d08bafa3560
ranelho <ranelhol@gmail.com>
2023-04-27 16:33:21 -0300

    corrige erros

--- src/main/java/com/rlti/autoescola/exame/domain/Validate.java
+++ src/main/java/com/rlti/autoescola/exame/domain/Validate.java
@@ -2,44 +2,48 @@ package com.rlti.autoescola.exame.domain;
 
 import com.rlti.autoescola.exame.application.api.ExameRequest;
 import com.rlti.autoescola.handler.APIException;
-import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
 import static com.rlti.autoescola.exame.domain.Resultado.*;
 import static com.rlti.autoescola.exame.domain.TipoExame.*;
+import static com.rlti.autoescola.handler.APIException.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 public class Validate {
     public static void validaExame(List<Exame> exames, ExameRequest request)  {
         if (exames.isEmpty() && request.getTipoExame() != CLINICO) {
-            throw APIException.build(HttpStatus.BAD_REQUEST,"O primeiro exame deve ser do tipo CLINICO");
+            throw build(BAD_REQUEST,"O primeiro exame deve ser do tipo CLINICO");
         }
         for (Exame exame : exames) {
             //valida exame clinico
-            if (request.getTipoExame() == CLINICO && exame.getTipoExame() == CLINICO && exame.getResultado() == APTO) {
-                throw APIException.build(HttpStatus.BAD_REQUEST,"Não é permitido cadastrar outro exame CLINICO");
-            }//OK
-            if (request.getTipoExame() == TEORICO && exame.getTipoExame() == TEORICO && exame.getResultado() == APTO){
-                throw APIException.build(HttpStatus.BAD_REQUEST,"Não é permitido cadastrar outro exame TEORICO");
-            }
-            if (request.getTipoExame() == PRATICO && exame.getTipoExame() == PRATICO && exame.getResultado() == APTO){
-                throw APIException.build(HttpStatus.BAD_REQUEST,"Não é permitido cadastrar outro exame PRATICO");
-            }
-            //valida exame clinico
             if(request.getTipoExame().equals(CLINICO)){
-                if ((exame.getTipoExame() == CLINICO && exame.getResultado() == INAPTO)
-                        || exame.getTipoExame() == CLINICO && exame.getResultado() != A_FAZER) {
+                if (exame.getTipoExame() == CLINICO && exame.getResultado() == APTO || exame.getResultado() == A_FAZER) {
+                    throw build(BAD_REQUEST,"Não é permitido cadastrar outro exame CLINICO");
+                }else {
                     continue;
                 }
             }
             //valida exame teorico
-            if ((request.getTipoExame() == TEORICO && exame.getTipoExame() == CLINICO && exame.getResultado() == APTO) || (request.getTipoExame() == TEORICO && exame.getTipoExame() == TEORICO && exame.getResultado() == INAPTO)) {
-                continue;
+            if(request.getTipoExame().equals(TEORICO)){
+                if (exame.getTipoExame() == TEORICO && exame.getResultado() == APTO || exame.getResultado() == A_FAZER){
+                    throw build(BAD_REQUEST,"Não é permitido cadastrar outro exame TEORICO");
+                }
+                if ((exame.getTipoExame() == CLINICO && exame.getResultado() == APTO)
+                || (exame.getTipoExame() == TEORICO && exame.getResultado() == INAPTO)){
+                    continue;
+                }
             }
-            if ((request.getTipoExame() == PRATICO && exame.getTipoExame() == TEORICO && exame.getResultado() == APTO) || ((request.getTipoExame() == PRATICO && exame.getTipoExame() == PRATICO && exame.getResultado() == INAPTO)) ){
-                continue;
+            //valida pratico
+            if(request.getTipoExame().equals(PRATICO)){
+                if (exame.getTipoExame() == PRATICO && exame.getResultado() == APTO || exame.getResultado() == A_FAZER){
+                    throw build(BAD_REQUEST,"Não é permitido cadastrar outro exame PRATICO");
+                }
+                if ((exame.getTipoExame() == TEORICO && exame.getResultado() == APTO)
+                    || (exame.getTipoExame() == PRATICO && exame.getResultado() == INAPTO)){
+                    continue;
+                }
             }
-
         }
     }
 }
