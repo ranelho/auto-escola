193fefde9a293fc0b3541b6cb765b9dc7b0cba44
ranelho <ranelhol@gmail.com>
2023-06-24 20:35:38 -0300

    alteracao nome e sobrenome

--- src/main/java/com/rlti/autoescola/security/auth/api/RegisterRequest.java
+++ src/main/java/com/rlti/autoescola/security/auth/api/RegisterRequest.java
@@ -13,8 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
-  private String firstname;
-  private String lastname;
+  private String firstName;
+  private String lastName;
   @Email
   private String email;
   private String password;
