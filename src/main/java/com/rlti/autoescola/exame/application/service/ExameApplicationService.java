5f3202b2e78361b56df3d7822edd2f05253dc49c
ranelho <ranelhol@gmail.com>
2023-07-11 16:33:04 -0300

    revalida exame

--- src/main/java/com/rlti/autoescola/exame/application/service/ExameApplicationService.java
+++ src/main/java/com/rlti/autoescola/exame/application/service/ExameApplicationService.java
@@ -1,10 +1,12 @@
 package com.rlti.autoescola.exame.application.service;
 
-import com.rlti.autoescola.exame.application.api.*;
+import com.rlti.autoescola.exame.application.api.ExameIdResponse;
+import com.rlti.autoescola.exame.application.api.ExameRequest;
+import com.rlti.autoescola.exame.application.api.ExameResponse;
+import com.rlti.autoescola.exame.application.api.ResultadoRequest;
 import com.rlti.autoescola.exame.application.repository.ExameRepository;
 import com.rlti.autoescola.exame.domain.Exame;
 import com.rlti.autoescola.exame.domain.Resultado;
-import com.rlti.autoescola.exame.domain.ValidaExame;
 import com.rlti.autoescola.handler.APIException;
 import com.rlti.autoescola.matricula.application.repository.MatriculaRepository;
 import com.rlti.autoescola.matricula.domain.Matricula;
@@ -19,6 +21,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static com.rlti.autoescola.exame.application.api.ExameResponse.*;
+import static com.rlti.autoescola.exame.domain.ValidaExame.validaExame;
+
 @Service
 @RequiredArgsConstructor
 @Log4j2
@@ -29,43 +34,43 @@ public class ExameApplicationService implements ExameService {
 
     @Override
     public ExameIdResponse saveExame(UUID idMatricula, ExameRequest record) {
-        log.info("[inicia] ExameApplicationService - saveExame");
+        log.info("[inicia] ExameApplicationService.saveExame");
         Matricula matricula = matriculaRepository.getOneMatricula(idMatricula);
         List<Exame> exames = exameRepository.getAllExamesByMatricula(matricula);
-        ValidaExame.validaExame(exames, record);
+        validaExame(exames, record);
         Exame exame = exameRepository.saveExame(new Exame(matricula, record));
-        log.info("[finaliza] ExameApplicationService - saveExame");
+        log.info("[finaliza] ExameApplicationService.saveExame");
         return ExameIdResponse.builder().idExame(exame.getIdExame()).build();
     }
 
     @Override
     public ExameResponse getOneExame(Long idExame) {
-        log.info("[inicia] ExameApplicationService - getOneExame");
+        log.info("[inicia] ExameApplicationService.getOneExame");
         Exame exame = exameRepository.getOneExame(idExame);
-        log.info("[finaliza] ExameApplicationService - getOneExame");
+        log.info("[finaliza] ExameApplicationService.getOneExame");
         return new ExameResponse(exame);
     }
 
     @Override
     public List<ExameResponse> getAllExames(UUID idMatricula) {
-        log.info("[inicia] ExameApplicationService - getAllExames");
+        log.info("[inicia] ExameApplicationService.getAllExames");
         Matricula matricula = matriculaRepository.getOneMatricula(idMatricula);
         List<Exame> exames = exameRepository.getAllExamesByMatricula(matricula);
-        log.info("[finaliza] ExameApplicationService - getAllExames");
-        return ExameResponse.converte(exames);
+        log.info("[finaliza] ExameApplicationService.getAllExames");
+        return converte(exames);
     }
 
     @Override
     public void deleteExame(Long idExame) {
         //TODO -> criar validacao permitir exclusão de um exame que quebre a norma
-        log.info("[inicia] ExameApplicationService - deleteExame");
+        log.info("[inicia] ExameApplicationService.deleteExame");
         exameRepository.deleteExame(exameRepository.getOneExame(idExame).getIdExame());
-        log.info("[finaliza] ExameApplicationService - deleteExame");
+        log.info("[finaliza] ExameApplicationService.deleteExame");
     }
 
     @Override
     public void updateExame(Long idExame, ResultadoRequest request) {
-        log.info("[inicia] ExameApplicationService - updateExame");
+        log.info("[inicia] ExameApplicationService.updateExame");
         Exame exame = exameRepository.getOneExame(idExame);
         if (!exame.getResultado().equals(Resultado.APTO)){
             exame.altera(request);
@@ -73,12 +78,12 @@ public class ExameApplicationService implements ExameService {
         }else {
             throw APIException.build(HttpStatus.BAD_REQUEST,"Exame finalizado!");
         }
-        log.info("[finaliza] ExameApplicationService - updateExame");
+        log.info("[finaliza] ExameApplicationService.updateExame");
     }
 
     @Override
     public List<ExameResponse> getAllExamesUser(String token) {
-        log.info("[inicia] ExameApplicationService - getAllExamesUser");
+        log.info("[inicia] ExameApplicationService.getAllExamesUser");
         List<Exame> exames;
         Optional<String> user = jwtService.getUserByBearerToken(token);
         if (user.isPresent() && user.get().equals(Role.USER.name())) {
@@ -87,7 +92,7 @@ public class ExameApplicationService implements ExameService {
         } else {
             exames = exameRepository.getAllExames();
         }
-        log.info("[finaliza] ExameApplicationService - getAllExamesUser");
-        return ExameResponse.converte(exames);
+        log.info("[finaliza] ExameApplicationService.getAllExamesUser");
+        return converte(exames);
     }
 }
