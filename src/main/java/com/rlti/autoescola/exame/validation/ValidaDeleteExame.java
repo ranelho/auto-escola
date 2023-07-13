047a6cfad8492bcc5a88793c64381e8069049039
ranelho <ranelhol@gmail.com>
2023-07-13 09:59:44 -0300

    update valida delete exame

--- src/main/java/com/rlti/autoescola/exame/validation/ValidaDeleteExame.java
+++ src/main/java/com/rlti/autoescola/exame/validation/ValidaDeleteExame.java
@@ -3,7 +3,6 @@ package com.rlti.autoescola.exame.validation;
 import com.rlti.autoescola.exame.domain.Exame;
 import com.rlti.autoescola.handler.APIException;
 import lombok.experimental.UtilityClass;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
@@ -11,43 +10,27 @@ import java.util.List;
 import static com.rlti.autoescola.exame.domain.Resultado.APTO;
 import static com.rlti.autoescola.exame.domain.TipoExame.*;
 
-@Log4j2
 @UtilityClass
 public class ValidaDeleteExame {
-
-    public static void validaDelete(List<Exame> exames, Exame oneExame) {
-        /*
-         * 1 - CLINICO -> SE CLINICO INAPTO PODE EXCLUIR
-         * 2 - TEORICO -> SE CLINICO APTO E TEORICO INAPTO PODE EXCLUIR
-         * 3 - PRATICO -> SE CLINICO APTO E TEORICO APTO E PRATICO INAPTO PODE EXCLUIR
-         * */
-        boolean hasClinicoApto = false;
-        boolean hasTeoricoApto = false;
-        boolean hasPraticoApto = false;
-        String mensagemAcaoNaoPermitida = "Ação não permitida!";
+    public void validaDelete(List<Exame> exames, Exame oneExame) {
+        boolean clinicoApto = false;
+        boolean teoricoApto = false;
+        boolean praticoApto = false;
 
         for (Exame exame : exames) {
-            log.info("[INICIO-FOR]");
-            if (exame.getTipoExame() == CLINICO && exame.getResultado() == APTO) {
-                log.info("[CLINICO] {}", exame.getIdExame());
-                hasClinicoApto = true;
+            if (exame.getTipoExame() == CLINICO) {
+                clinicoApto = exame.getResultado() == APTO;
+            } else if (exame.getTipoExame() == TEORICO) {
+                teoricoApto = exame.getResultado() == APTO;
+            } else if (exame.getTipoExame() == PRATICO) {
+                praticoApto = exame.getResultado() == APTO;
             }
-            if (exame.getTipoExame() == TEORICO && exame.getResultado() == APTO) {
-                log.info("[TEORICO] {}", exame.getIdExame());
-                hasTeoricoApto = true;
-            }
-            if (exame.getTipoExame() == PRATICO && exame.getResultado() == APTO) {
-                log.info("[PRATICO] {}", exame.getIdExame());
-                hasPraticoApto = true;
-            }
-            log.info("[FIM-FOR]");
         }
 
-        log.info("[INICIO-VERIFICAÇÃO]");
-        if ((hasPraticoApto && hasClinicoApto && hasTeoricoApto) ||
-                (oneExame.getTipoExame().equals(CLINICO) && hasClinicoApto) ||
-                (oneExame.getTipoExame().equals(TEORICO) && hasTeoricoApto)) {
-            throw APIException.build(HttpStatus.BAD_REQUEST, mensagemAcaoNaoPermitida);
+        if ((praticoApto && clinicoApto && teoricoApto) ||
+                (oneExame.getTipoExame().equals(CLINICO) && clinicoApto) ||
+                (oneExame.getTipoExame().equals(TEORICO) && teoricoApto)) {
+            throw APIException.build(HttpStatus.BAD_REQUEST, "Ação não permitida!");
         }
     }
 }
