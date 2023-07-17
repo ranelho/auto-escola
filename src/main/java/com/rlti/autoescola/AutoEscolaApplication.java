318070110cb69d909ae5f73cca4bba0eca4e6f5c
ranelho <ranelhol@gmail.com>
2023-07-17 20:04:36 -0300

    add data criaçao e data atualizao

--- src/main/java/com/rlti/autoescola/AutoEscolaApplication.java
+++ src/main/java/com/rlti/autoescola/AutoEscolaApplication.java
@@ -5,10 +5,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableJpaAuditing
 @OpenAPIDefinition(info = @Info(title = "Auto Escola API", version = "1.0.0", description = "Gestão de Auto Escola"),
 		servers = {@Server(url = "/auto-escola/api", description = "Default Server URL")})
 public class AutoEscolaApplication {
--- src/main/java/com/rlti/autoescola/cliente/application/api/ClienteAPI.java
+++ src/main/java/com/rlti/autoescola/cliente/application/api/ClienteAPI.java
@@ -2,7 +2,6 @@ package com.rlti.autoescola.cliente.application.api;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,31 +10,34 @@ import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
+import static org.springframework.http.HttpStatus.*;
+
 @Tag(name = "Cliente", description = "Cliente APIs")
 @RequestMapping("/v1/clientes")
 public interface ClienteAPI {
     @PostMapping
-    @ResponseStatus(code = HttpStatus.CREATED)
+    @ResponseStatus(code = CREATED)
     ClienteResponse saveCliente(@Valid @RequestBody ClienteRequest clienteRequest);
 
-    @GetMapping(value = "/{idCliente}")
-    @ResponseStatus(code = HttpStatus.OK)
+    @GetMapping("/{idCliente}")
+    @ResponseStatus(code = OK)
     ClienteResponse getOneCliente(@PathVariable UUID idCliente);
 
-    @GetMapping(value = "/cpf")
-    @ResponseStatus(code = HttpStatus.OK)
+    @GetMapping("/cpf")
+    @ResponseStatus(code = OK)
     ClienteResponse getByCpf(@RequestParam String cpf);
 
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/all")
-    @ResponseStatus(code = HttpStatus.OK)
+    @ResponseStatus(code = OK)
     List<ClienteListResponse> getAllClientes();
 
     @PatchMapping("/{idCliente}")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @ResponseStatus(code = NO_CONTENT)
     void updateCliente(@PathVariable UUID idCliente, @Valid @RequestBody EditaClienteRequest editaClienteRequest);
 
     @PutMapping("/{idCliente}/foto")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @ResponseStatus(code = NO_CONTENT)
     void saveImagem(@PathVariable UUID idCliente, @RequestParam("foto") MultipartFile imagem) throws IOException;
+
 }
